6e92818b784285dc6988d1a576f2b8156d1937a3
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-05 16:38:32 +0200

    control errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
   const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -26,6 +28,24 @@ function App() {
     updateSearch(newSearch);
   };
 
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
   return (
     <div className="page">
       <header>
@@ -39,6 +59,7 @@ function App() {
           />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p className="error">{error}</p>}
       </header>
 
       <main>
