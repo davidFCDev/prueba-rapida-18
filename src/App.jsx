8fa028f7973ce351caa722e133c29c380f5d225a
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-05 16:37:14 +0200

    control eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -14,12 +16,27 @@ function App() {
     };
   });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Buscando películas...");
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input type="text" placeholder="Busca una película" />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            type="text"
+            placeholder="Busca una película"
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
